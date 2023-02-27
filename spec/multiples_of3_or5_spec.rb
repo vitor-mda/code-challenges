4104df1e25960149a0e22408644db24db53b1bf7
vitor-mda <vitor-mda@hotmail.com>
2023-02-27 00:54:17 -0300

    🧪 test: add spec for `MultiplesOf3Or5`

---
+++ spec/multiples_of3_or5_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'multiples_of3_or5/multiples_of3_or5'
+
+RSpec.describe MultiplesOf3Or5 do
+  describe '.sum_of_multiples_below' do
+    subject { described_class.sum_of_multiples_below(ceiling) }
+
+    context 'when ceiling is 10' do
+      let(:ceiling) { 10 }
+
+      it { is_expected.to eq 23 }
+    end
+
+    context 'when ceiling is 1000' do
+      let(:ceiling) { 1000 }
+
+      it { is_expected.to eq 233_168 }
+    end
+  end
+end
