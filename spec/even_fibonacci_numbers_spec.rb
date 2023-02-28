79f5edbcc4b9a15f5af7136ea36808a008b993dd
vitor-mda <vitor-mda@hotmail.com>
2023-02-28 00:56:48 -0300

    🧪 test: add spec for `EvenFibonacciNumbers`

---
+++ spec/even_fibonacci_numbers_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'even_fibonacci_numbers/even_fibonacci_numbers'
+
+RSpec.describe EvenFibonacciNumbers do
+  describe '.sum_even_terms_below' do
+    subject { described_class.sum_even_terms_below(ceiling) }
+
+    context 'when ceiling is 50' do
+      let(:ceiling) { 50 }
+
+      it { is_expected.to eq 44 }
+    end
+
+    context 'when ceiling is 4_000_000' do
+      let(:ceiling) { 4_000_000 }
+
+      it { is_expected.to eq 4_613_732 }
+    end
+  end
+end
