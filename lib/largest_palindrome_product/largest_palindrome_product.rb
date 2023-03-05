6bf994b82655d0a1dc1a985be36286d19fcc5cbd
vitor-mda <vitor-mda@hotmail.com>
2023-03-05 00:00:12 -0300

    ⚡️ perf: make `LargestPalindromeProduct` stateful
    Slightly improves performance by calculating the lower bound only once

--- lib/largest_palindrome_product/largest_palindrome_product.rb
+++ lib/largest_palindrome_product/largest_palindrome_product.rb
@@ -1,41 +1,48 @@
 # frozen_string_literal: true
 
 class LargestPalindromeProduct
-  class << self
-    def from_two_numbers_with(digits:)
-      numbers_sequence_from(digits).filter_map do |n1|
-        n1.downto(lower_bound(digits)).filter_map do |n2|
-          (n1 * n2).then { |result| result if palindrome?(result) }
-        end.max
+  def self.from_two_numbers_with(digits:)
+    new(digits).execute
+  end
+
+  def execute
+    numbers_sequence.filter_map do |n1|
+      n1.downto(@lower_bound).filter_map do |n2|
+        (n1 * n2).then { |result| result if palindrome?(result) }
       end.max
-    end
+    end.max
+  end
 
-    private
+  private
 
-    def numbers_sequence_from(digits)
-      upper_bound(digits).downto(lower_bound(digits))
-    end
+  def initialize(digits)
+    @upper_bound = upper_bound(digits)
+    @lower_bound = lower_bound(digits)
+  end
 
-    def palindrome?(number)
-      number.digits.then { |digits| digits == digits.reverse }
-    end
+  def numbers_sequence
+    @upper_bound.downto @lower_bound
+  end
 
-    def upper_bound(digits)
-      return 9 if digits == 1
+  def palindrome?(number)
+    number.digits.then { |digits| digits == digits.reverse }
+  end
 
-      calculate_bound(9, digits) { |n| (n * 10) + 9 }
-    end
+  def upper_bound(digits)
+    return 9 if digits == 1
 
-    def lower_bound(digits)
-      return 1 if digits == 1
+    calculate_bound(9, digits) { |n| (n * 10) + 9 }
+  end
 
-      calculate_bound(1, digits) { |n| n * 10 }
-    end
+  def lower_bound(digits)
+    return 1 if digits == 1
 
-    def calculate_bound(number, digits, &block)
-      return calculate_bound(block.call(number), digits, &block) if number.digits.length < digits
+    calculate_bound(1, digits) { |n| n * 10 }
+  end
 
-      number
-    end
+  def calculate_bound(number, digits, &block)
+    return calculate_bound(block.call(number), digits, &block) if number.digits.length < digits
+
+    number
   end
 end
