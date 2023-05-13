9458ae93217f04a956d0e8eadf8a3e531bcf7847
vitor-mda <vitor-mda@hotmail.com>
2023-05-13 14:03:35 -0300

    ⚡️ perf: add smaller upper bound for inner loop
    Kind of ad-hoc, but performs better for the examples.

--- lib/special_pythagorean_triplet/special_pythagorean_triplet.rb
+++ lib/special_pythagorean_triplet/special_pythagorean_triplet.rb
@@ -3,8 +3,10 @@
 class SpecialPythagoreanTriplet
   class << self
     def product_of_a_triplet_which_the_sum_equals(sum)
-      (1..sum).each do |a|
-        (a + 1..sum).each do |b|
+      upper_bound = sum / 2
+
+      (1..).each do |a|
+        (a + 1..upper_bound).each do |b|
           c = sum - (a + b)
           return (a * b * c) if (a**2) + (b**2) == c**2
         end
