d7493f71ec008efd88febeec51c8d1a4c46fa646
vitor-mda <vitor-mda@hotmail.com>
2024-04-14 23:00:48 -0300

    ⚡️ perf: memoize Collatz sequences sizes

--- lib/longest_collatz_sequence/longest_collatz_sequence.rb
+++ lib/longest_collatz_sequence/longest_collatz_sequence.rb
@@ -1,34 +1,55 @@
 # frozen_string_literal: true
 
 class LongestCollatzSequence
-  class << self
-    def longest_chain_producer_under(max_starting_number)
-      longest_chain_producer = 0
-      longest_chain_size = 0
+  def self.longest_chain_producer_under(max_starting_number) = new(max_starting_number).find
 
-      (1...max_starting_number).each do |starting_number|
-        chain_size = chain_size_for(starting_number)
-        if chain_size > longest_chain_size
-          longest_chain_producer = starting_number
-          longest_chain_size = chain_size
-        end
+  def initialize(max_starting_number)
+    @max_starting_number = max_starting_number
+    @chain_size_by_number = { 1 => 1 }
+    @longest_chain_producer = 1
+    @longest_chain_size = 1
+  end
+
+  def find
+    calculate_longest_producer
+
+    @longest_chain_producer
+  end
+
+  private
+
+  def calculate_longest_producer
+    starting_numbers.each do |starting_number|
+      chain_size_for(starting_number).tap do |size|
+        update(longest_chain_producer: starting_number, longest_chain_size: size) if size > @longest_chain_size
       end
+    end
+  end
 
-      longest_chain_producer
+  def starting_numbers = (1...@max_starting_number)
+
+  def chain_size_for(number)
+    size = 0
+    last_number = number
+
+    loop do
+      break size += memoized(last_number) if memoized?(last_number)
+
+      size += 1
+      last_number = next_for(last_number)
     end
 
-    private
+    memoize(number, with: size)
+    size
+  end
 
-    def chain_size_for(number)
-      size = 1
-      last_number = number
+  def memoized?(number)      = @chain_size_by_number.key?(number)
+  def memoized(number)       = @chain_size_by_number[number]
+  def memoize(number, with:) = @chain_size_by_number[number] = with
+  def next_for(number)       = number.even? ? number / 2 : (3 * number) + 1
 
-      until last_number == 1
-        size += 1
-        last_number = last_number.even? ? last_number / 2 : (3 * last_number) + 1
-      end
-
-      size
-    end
+  def update(longest_chain_producer:, longest_chain_size:)
+    @longest_chain_producer = longest_chain_producer
+    @longest_chain_size = longest_chain_size
   end
 end
