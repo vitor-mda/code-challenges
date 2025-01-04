0858dbf48869639f3a1ee00adf8698480921272f
vitor-mda <vitor-mda@hotmail.com>
2025-01-04 18:14:32 -0300

    ✨ feat: add own Largest Prime Factor solution, replacing gem-sourced one

--- lib/largest_prime_factor/largest_prime_factor.rb
+++ lib/largest_prime_factor/largest_prime_factor.rb
@@ -1,11 +1,33 @@
 # frozen_string_literal: true
 
-require 'prime'
-
 class LargestPrimeFactor
   class << self
-    def of(number)
-      Prime.prime_division(number).max.first
+    def of(number) = new(number).find
+  end
+
+  def initialize(number)
+    @number = number
+    @largest_prime_factor = 2
+    @largest_prime_factor_candidate = 2
+  end
+
+  def find
+    calculate_largest_prime_factor
+    @largest_prime_factor
+  end
+
+  private
+
+  def calculate_largest_prime_factor
+    while candidate_below_number?
+      promote_candidate && reduce_number if candidate_is_a_factor?
+      next_candidate
     end
   end
+
+  def candidate_below_number? = @largest_prime_factor_candidate <= @number
+  def candidate_is_a_factor?  = (@number % @largest_prime_factor_candidate).zero?
+  def promote_candidate       = @largest_prime_factor = @largest_prime_factor_candidate
+  def reduce_number           = (@number /= @largest_prime_factor_candidate while candidate_is_a_factor?)
+  def next_candidate          = @largest_prime_factor_candidate += 1
 end
